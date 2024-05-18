34978b93a8fa9c903ea12ac83b3b0968b70bd769
AbelVega19 <vegarenteriacesar@gmail.com>
2024-05-18 16:46:09 -0700

    Guardar-Receta

---
+++ html/guardar_receta.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'conexion.php'; 
+
+// Obtener los datos del formulario
+$nombreReceta = $_POST['nombre_receta'];
+$pasosReceta = $_POST['pasos'];
+$caloriasTotales = $_POST['calorias_totales'];
+$proteinasTotales = $_POST['proteinas_totales'];
+$lipidosTotales = $_POST['lipidos_totales'];
+$hidratosDeCarbonoTotales = $_POST['hidratos_de_carbono_totales'];
+$ingredientesReceta = $_POST['ingredientes'];
+$ruta1 = $_POST['ruta1'];
+$ruta2 = $_POST['ruta2'];
+$ruta3 = $_POST['ruta3'];
+$usu_id = $_POST['usu_id'];
+
+try {
+    $conexion = Conexion::conectar();
+    $consulta = $conexion->prepare("INSERT INTO Recetas (nombre_receta, pasos, calorias_totales, proteinas_totales, lipidos_totales, hidratos_de_carbono_totales, ingredientes, ruta1, ruta2, ruta3, usu_id) VALUES (:nombre_receta, :pasos, :calorias_totales, :proteinas_totales, :lipidos_totales, :hidratos_de_carbono_totales, :ingredientes, :ruta1, :ruta2, :ruta3, :usu_id)");
+    $consulta->bindParam(':nombre_receta', $nombreReceta);
+    $consulta->bindParam(':pasos', $pasosReceta);
+    $consulta->bindParam(':calorias_totales', $caloriasTotales);
+    $consulta->bindParam(':proteinas_totales', $proteinasTotales);
+    $consulta->bindParam(':lipidos_totales', $lipidosTotales);
+    $consulta->bindParam(':hidratos_de_carbono_totales', $hidratosDeCarbonoTotales);
+    $consulta->bindParam(':ingredientes', $ingredientesReceta);
+    $consulta->bindParam(':ruta1', $ruta1);
+    $consulta->bindParam(':ruta2', $ruta2);
+    $consulta->bindParam(':ruta3', $ruta3);
+    $consulta->bindParam(':usu_id', $usu_id);
+    
+    $conexion->beginTransaction();
+
+    $consulta->execute();
+
+    // Eliminar el contenido de la tabla imagenes
+    $consultaEliminar = $conexion->prepare("DELETE FROM imagenes");
+    $consultaEliminar->execute();
+
+    $conexion->commit();
+
+    echo "Receta guardada exitosamente";
+
+} catch (PDOException $e) {
+    $conexion->rollBack();
+    echo "Error al guardar la receta: " . $e->getMessage();
+}
+?>
--- html/nueva-receta.php
+++ html/nueva-receta.php
@@ -1,7 +1,20 @@
 <?php
 require_once 'conexion.php';
 include_once 'obtener_datos.php';
+
+$conn = Conexion::conectar();
+
+$sql = "SELECT Nombre, Calorias, Proteinas, Lipidos, Hidratos_de_Carbono FROM Ingredientes";
+$result = $conn->query($sql);
+
+$ingredientes = [];
+if ($result->rowCount() > 0) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $ingredientes[] = $row;
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,9 +27,7 @@ include_once 'obtener_datos.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>Nutricraft Eats</title>
 </head>
 
@@ -64,97 +75,219 @@ include_once 'obtener_datos.php';
                         </button>
                     </div>
                 </form>
-        
+
                 <div class="contenedor-imagenes">
                     <?php
-                    require_once 'conexion.php'; // Incluye el archivo de conexión a la base de datos
-        
-                    // Consulta las rutas de las imágenes desde la base de datos
-                    $conn = Conexion::conectar();
-                    $stmt = $conn->query("SELECT ruta FROM imagenes");
-                    $rutasImagenes = $stmt->fetchAll(PDO::FETCH_COLUMN);
-                    $conn = null; // Cierra la conexión
-        
-                    // Genera las etiquetas <img> para cada ruta de imagen
-                    foreach ($rutasImagenes as $ruta) {
-                        echo "<img src='$ruta'>";
+                    require_once 'conexion.php';
+
+                    try {
+                        $conn = Conexion::conectar();
+
+                        $stmt = $conn->prepare("SELECT ruta FROM imagenes ORDER BY fecha_subida DESC LIMIT 3");
+                        $stmt->execute();
+                        $rutasImagenes = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+                        foreach ($rutasImagenes as $ruta) {
+                            echo "<img src='$ruta'>";
+                        }
+
+                        $conn = null; 
+                    } catch (PDOException $e) {
+                        echo "Error al obtener las imágenes: " . $e->getMessage();
                     }
                     ?>
                 </div>
             </div>
-
+            <style>
+                #lista-ingredientes {
+                    display: none;
+                }
+            </style>
             <div class="zona-2">
                 <form>
-                    <input type="text" id="nombre-receta" name="nombre-receta"
-                        placeholder="Escribe el nombre de tu receta">
+                    <input type="text" id="nombre-receta" name="nombre-receta" placeholder="Escribe el nombre de tu receta">
                     <label for="ingredientes">Ingredientes:</label>
                     <textarea id="ingredientes" name="ingredientes" rows="4" class="lista-ing"></textarea>
                     <ul id="lista-ingredientes">
-                        <!-- La lista de ingredientes aparecerá aquí -->
+                        
                     </ul>
                     <div class="contenedor-botones">
                         <button type="button" id="agregar-ingrediente">+</button>
                         <button type="button" id="eliminar-ingrediente" class="less">-</button>
-                        
                     </div>
                     <div class="guardar">
                         <button type="button" id="openModalBtn" class="mandar-receta">Modal</button>
-                        <button type="button" class="mandar-receta">
-                            Subir receta
-                        </button>
+                        <button type="button" id="subir-receta" class="mandar-receta">Subir receta</button>
                     </div>
                 </form>
 
                 <div class="zona-2-2">
-                    <textarea id="pasos-receta" class="pasos-receta"
-                        placeholder="Escriba aqui todos los pasos de su receta en orden"></textarea>
+                    <textarea id="pasos-receta" class="pasos-receta" placeholder="Escriba aqui todos los pasos de su receta en orden"></textarea>
                     <ul id="nutrimentos-ing" class="nutrimentos-ingredientes">
                         <i>Nutrimentos registrados:</i>
                     </ul>
                 </div>
-
             </div>
 
-        </div>
-
-    </div>
-
-    <div id="myModal" class="modal">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <h1>Agrega tus ingredientes</h1>
-            <div class="ingredientes-form">
-                <div class="zona1">
-                    <input type="text" placeholder="Busca los ingredientes aqui...">
-                    <button><i class="fa fa-fw fa-search"></i></button>
-                </div>
-                <div class="zona2">
-                    <select name="ingredientes" id="ingredientes">
-                        <option value="lechuga">Lechuga</option>
-                        <option value="lechuga">Lechuga</option>
-                        <option value="lechuga">Lechuga</option>
-                        <option value="lechuga">Lechuga</option>
-                    </select>
-                    <button><i class="fa fa-fw fa-plus-square"></i></button>
-                </div>
-                <div class="zona3">
-                    <label for="">Ingredientes agregados</label>
-                    <ul>
-                        <li>Lechuga</li>
-                        <li>Tomate</li>
-                        <li>Cebolla</li>
-                        <li>Zanahoria</li>
-                        <li>Pepino</li>
-                        <li>Chile</li>
-                    </ul>
+            <div id="myModal" class="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <h1>Agrega tus ingredientes</h1>
+                    <div class="ingredientes-form">
+                        <div class="zona1">
+                            <input type="text" placeholder="Busca los ingredientes aquí...">
+                            <button><i class="fa fa-fw fa-search"></i></button>
+                        </div>
+                        <div class="zona2">
+                            <select name="ingredientes" id="modal-ingredientes">
+                                <?php
+                                foreach ($ingredientes as $ingrediente) {
+                                    echo "<option value='" . htmlspecialchars(json_encode($ingrediente)) . "'>" . htmlspecialchars($ingrediente['Nombre']) . "</option>";
+                                }
+                                ?>
+                            </select>
+                            <button id="modal-agregar-ingrediente"><i class="fa fa-fw fa-plus-square"></i></button>
+                        </div>
+                        <div class="zona3">
+                            <label for="">Ingredientes agregados</label>
+                            <ul id="modal-lista-ingredientes">
+                                
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <script src="/js/modal.js"></script>
-    <script src="/js/nueva-receta.js"></script>
-    <script src="/js/nav-bar.js"></script>
+            <input type="hidden" id="calorias-totales" name="calorias_totales" value="">
+            <input type="hidden" id="proteinas-totales" name="proteinas_totales" value="">
+            <input type="hidden" id="lipidos-totales" name="lipidos_totales" value="">
+            <input type="hidden" id="hidratos-carbono-totales" name="hidratos_de_carbono_totales" value="">
+
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var listaIngredientes = document.getElementById('lista-ingredientes');
+                    var ingredientesTextarea = document.getElementById('ingredientes');
+                    var selectIngredientes = document.getElementById('select-ingredientes');
+                    var agregarIngredienteBtn = document.getElementById('agregar-ingrediente');
+                    var nutrimentosUl = document.getElementById('nutrimentos-ing');
+                    var subirRecetaBtn = document.getElementById('subir-receta');
+                    var modal = document.getElementById('myModal');
+                    var modalAgregarBtn = document.getElementById('modal-agregar-ingrediente');
+                    var modalListaIngredientes = document.getElementById('modal-lista-ingredientes');
+                    var modalIngredientesSelect = document.getElementById('modal-ingredientes');
+                    var openModalBtn = document.getElementById('openModalBtn');
+                    var closeModalBtn = document.querySelector('.modal .close');
+
+                    var valoresNutricionalesTotales = {
+                        Calorias: 0,
+                        Proteinas: 0,
+                        Lipidos: 0,
+                        Hidratos_de_Carbono: 0
+                    };
+
+                    function agregarIngrediente(ingrediente) {
+                        var nuevoIngrediente = ingrediente.Nombre;
+
+                        var li = document.createElement('li');
+                        li.textContent = nuevoIngrediente;
+                        listaIngredientes.appendChild(li);
+                        ingredientesTextarea.value += (ingredientesTextarea.value ? '\n' : '') + nuevoIngrediente;
+
+                        valoresNutricionalesTotales.Calorias += ingrediente.Calorias;
+                        valoresNutricionalesTotales.Proteinas += ingrediente.Proteinas;
+                        valoresNutricionalesTotales.Lipidos += ingrediente.Lipidos;
+                        valoresNutricionalesTotales.Hidratos_de_Carbono += ingrediente.Hidratos_de_Carbono;
+
+                        mostrarValoresNutricionales();
+                    }
+
+                    function mostrarValoresNutricionales() {
+                        nutrimentosUl.innerHTML = "<i>Nutrimentos registrados:</i><br>";
+                        for (var nutrimento in valoresNutricionalesTotales) {
+                            nutrimentosUl.innerHTML += "<li>" + nutrimento + ": " + valoresNutricionalesTotales[nutrimento] + "</li>";
+                        }
+                    }
+
+                    agregarIngredienteBtn.addEventListener('click', function() {
+                        var selectedOption = selectIngredientes.options[selectIngredientes.selectedIndex];
+                        if (selectedOption) {
+                            var ingrediente = JSON.parse(selectedOption.value);
+                            agregarIngrediente(ingrediente);
+                        }
+                    });
+
+                    modalAgregarBtn.addEventListener('click', function() {
+                        var selectedOption = modalIngredientesSelect.options[modalIngredientesSelect.selectedIndex];
+                        if (selectedOption) {
+                            var ingrediente = JSON.parse(selectedOption.value);
+
+                            var li = document.createElement('li');
+                            li.textContent = ingrediente.Nombre;
+                            modalListaIngredientes.appendChild(li);
+
+                            agregarIngrediente(ingrediente);
+                        }
+                    });
+
+                    openModalBtn.addEventListener('click', function() {
+                        modal.style.display = 'block';
+                    });
+
+                    closeModalBtn.addEventListener('click', function() {
+                        modal.style.display = 'none';
+                    });
+
+                    subirRecetaBtn.addEventListener('click', function() {
+                        var nombreReceta = document.getElementById('nombre-receta').value.trim();
+                        var pasosReceta = document.getElementById('pasos-receta').value.trim();
+                        var ingredientesReceta = ingredientesTextarea.value.trim();
+                        var caloriasTotales = valoresNutricionalesTotales.Calorias;
+                        var proteinasTotales = valoresNutricionalesTotales.Proteinas;
+                        var lipidosTotales = valoresNutricionalesTotales.Lipidos;
+                        var hidratosDeCarbonoTotales = valoresNutricionalesTotales.Hidratos_de_Carbono;
+
+                        var rutasImagenes = [];
+                        document.querySelectorAll('.contenedor-imagenes img').forEach(function(img, index) {
+                            if (index < 3) { // Tomamos hasta tres imágenes
+                                rutasImagenes.push(img.src);
+                            }
+                        });
+
+                        while (rutasImagenes.length < 3) {
+                            rutasImagenes.push('');
+                        }
+
+                        $.ajax({
+                            type: "POST",
+                            url: "guardar_receta.php",
+                            data: {
+                                nombre_receta: nombreReceta,
+                                pasos: pasosReceta,
+                                ingredientes: ingredientesReceta,
+                                calorias_totales: caloriasTotales,
+                                proteinas_totales: proteinasTotales,
+                                lipidos_totales: lipidosTotales,
+                                hidratos_de_carbono_totales: hidratosDeCarbonoTotales,
+                                ruta1: rutasImagenes[0],
+                                ruta2: rutasImagenes[1],
+                                ruta3: rutasImagenes[2],
+                                usu_id: <?php echo $_SESSION['usu_id']; ?>
+                            },
+                            success: function(response) {
+                                alert(response);
+                                window.location.href = "nueva-receta.php";
+                            },
+                            error: function(xhr, textStatus, errorThrown) {
+                                alert("Error al subir la receta: " + errorThrown);
+                            }
+                        });
+                    });
+                });
+            </script>
+            <script src="/js/modal.js"></script>
+            <script src="/js/nueva-receta.js"></script>
+            <script src="/js/nav-bar.js"></script>
 
 </body>
 
